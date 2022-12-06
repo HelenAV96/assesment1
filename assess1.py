080b9c0bc1236d7dfd24128b5c58d3ccbe842598
HelenAV96 <helenalbandak@hotmail.com>
2022-12-06 12:07:15 +0100

    fixade

--- assess1.py
+++ assess1.py
@@ -1,10 +1,11 @@
 import psycopg2
 
-conn = psycopg2.connect(
-    host='localhost',
-    database='assessmentdb',
-    user= 'postgres',
-    password= 'musikklass96')
+def get_db_connection():
+    conn = psycopg2.connect(
+        host='localhost',
+        database='assessmentdb',
+        user= 'postgres',
+        password= 'musikklass96')
 
 
 def list_data():
@@ -17,18 +18,38 @@ def list_data():
     con.close()
     return rows
 
-def add_contact(first_name, last_name):
+def add_contact(first_name, last_name, title, organization):
     con = get_db_connection()
     cur = con.cursor()
-    cur.execute(f"INSERT INTO contacts VALUES ('{first_name}','{last_name}');")
+    cur.execute(f"INSERT INTO contacts VALUES ('{first_name}','{last_name}','{title}','{organization}');")
     cur.execute("COMMIT;")
     cur.close()
     con.close()
 
-def delete_contact(name):
+def delete_contact(first_name, last_name):
     con = get_db_connection()
     cur = con.cursor()
-    cur.execute(f"DELETE FROM contacts WHERE name = '{name}';")
+    cur.execute(f"DELETE FROM contacts WHERE first_name = '{first_name}' and last_name ='{last_name}';")
     cur.execute("COMMIT;")
     cur.close()
     con.close()
+
+while True:
+    print("Hello and welcome to contacts! Choose between the following command: LIST, INSERT,DELETE")
+    command = input("What would you like to do?:")
+
+    if command == INSERT:
+        first_name = input("First name: ")
+        last_name = input("Last name: ")
+        title =  input("Title: ")
+        organization = input("organization: ")
+        add_contact()
+
+    elif command == DELETE:
+        first_name = input("First name: ")
+        last_name = input("Last name: ")
+        delete_contact()
+
+    elif command == LIST:
+        list_data()
+
--- assessmentdb.sql
+++ assessmentdb.sql
@@ -11,7 +11,65 @@ CREATE DATABASE assessmentdb
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
+
+CREATE TABLE contact_categories (
+    id SERIAL NOT NULL PRIMARY KEY,
+    contact_category VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE contact_types (
+    id SERIAL NOT NULL PRIMARY KEY,
+    contact_type VARCHAR(80) NOT NULL
+
+CREATE TABLE contacts (
+    id SERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(80) NOT NULL,
+    last_name VARCHAR(80) NOT NULL,
+    title VARCHAR(80),
+    organization VARCHAR(80)
+);
+
+CREATE TABLE items (
+    contact VARCHAR(80) NOT NULL,
+    contact_id integer NOT NULL,
+    contact_type_id integer,
+    contact_category_id integer
+);
 	
+
+INSERT INTO items (
+contact, contact_id, contact_type_id, contact_category_id) 
+VALUES ('011-12 33 45',3,2,1),
+('goran@infoab.se',3,1,2),
+('010-88 55 44',4,2,2),
+('erik57@hotmail.com',1,1,1)
+('@annapanna99',2,4,1)
+('077-563578',2,2,1)
+('070-156 22 78',3,2,2)
+;
+
+
+
+INSERT INTO contact_types( contact, contact_id, contact_type_id, contact_category_id)
+VALUES ('Home'), ('Work'), ('Fax');
+
+
+INSERT INTO contact_categories( contact_category)
+VALUES ('Home'), ('Work'), ('Fax');
+
+INSERT INTO contacts(first_name, last_name, title, organization)
+VALUES ('Erik', 'Eriksson','Teacher','Utbildning AB'),                                                                   
+('Anna','Sundh', null,null),
+('Ann-Marie','Bergqvist','Cousin', null),
+('Herman','Appelkvist',null, null),
+('Goran','Bregovic','Coach','Dalens IK';
+
+INSERT INTO contacts(first_name, last_name, title, organization)
+VALUES ('Helen', 'Albandak Vähäkangas','Data Engineer','Academic work'),                                                                   
+('Henrik','Vähäkangas', 'Vakthavande Befäl','Kriminalvården');
+
+
+
 --1.6	
 SELECT * FROM contact_types
 WHERE id NOT IN (SELECT contact_type_id
